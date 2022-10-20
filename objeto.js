62993b58d3a4cc6c0ef2ad303b64ca8af2272de7
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-19 21:07:23 -0300

    criando repositorio para aula de objetos

---
+++ objeto.js
@@ -0,0 +1,3 @@
+//O que é um objeteto.
+
+const listaCPFs = ["11235849","884520136","554130258"];
--- objetos.js
+++
@@ -1,3 +0,0 @@
-//novo curso. Objetos
-const listaCPFs = ["112534982", "225640235", "558430214"];
-
