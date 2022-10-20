de131248437a92a30634168af56898eed51f4068
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-20 19:09:25 -0300

    Adicionando e alterando

---
+++ adicionar-campos.js
@@ -0,0 +1,20 @@
+// Desafio - Adicionar campos
+
+// Adicionar informações importantes que ainda estão faltando, como telefone e também seus valores.
+
+const cliente = { 
+    nome:"Andre",
+    idade: 36,
+    cpf: "115478523",
+    email: "teste@alura.com.br"
+}
+
+console.log(cliente);
+
+cliente.fone = "83996073938"
+
+console.log(cliente);
+
+cliente.fone = "81994236574"
+
+console.log(cliente);
