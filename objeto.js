00712fccb523949f79a7eab99fe9f115678e1864
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-19 21:16:43 -0300

    Objeto é uma estrutura de chaves e valores

--- objeto.js
+++ objeto.js
@@ -1,3 +1,12 @@
 //O que é um objeteto.
 
 const listaCPFs = ["11235849","884520136","554130258"];
+
+//const cliente = ["nome","Andre","idade",36]; //arrays serve para armazenar informaçoes do mesmo tipo, ou só CPF, ou só nome, ou e-mail, etc
+
+const cliente = { //objetivo tem uma estrura de chave e valores, que pode armazenar várias informações ao mesmo tempo.
+    nome:"Andre";
+    idade: 36;
+    cpf: 115478523;
+    email: teste@alura.com.br;
+}
